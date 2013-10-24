24ac57b31687c6d8d2747eda8ab82297c2ce2990
Abhinav Asthana <rickyast@gmail.com>
2013-10-24 08:53:01 +0530

    For #374 and #40, Run tests after a Postman request

--- chrome/background.js
+++ chrome/background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('test_runner.html', {
+  chrome.app.window.create('requester.html', {
     "bounds": {
       width: 1200,
       height: 800
