edff01a349b6fb617b9b8b579efa231bd62ccc17
Abhinav Asthana <rickyast@gmail.com>
2013-11-20 11:25:37 +0530

    Changes for v0.9.5

---
+++ chrome/html/test_runner/templates.js
@@ -0,0 +1,11 @@
+this["Handlebars"] = this["Handlebars"] || {};
+this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};
+
+this["Handlebars"]["templates"]["item_test"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "";
+
+
+  return buffer;
+  });
