a0335c0ff78ce763e0f6c9cc5abe833b1c960c11
Abhinav Asthana <rickyast@gmail.com>
2013-11-25 16:04:26 +0530

    Maximizing/minimizing sidebar

--- chrome/js/test_runner/modules/sidebar/TestRunerSidebarState.js
+++ chrome/js/test_runner/modules/sidebar/TestRunerSidebarState.js
@@ -1,6 +1,7 @@
 var TestRunnerSidebarState = Backbone.Model.extend({
 	defaults: function() {
 		return {
+			"isSidebarMaximized": true,
 			"collections": null,
 			"envManager": null,
 			"testRuns": null
