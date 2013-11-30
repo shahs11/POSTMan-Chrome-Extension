0f618150b65630d5f9050fb80944844fbbe727b7
Abhinav Asthana <rickyast@gmail.com>
2013-11-30 19:09:54 +0530

    Updates

--- chrome/js/test_runner.js
+++ chrome/js/test_runner.js
@@ -557,7 +557,6 @@ var TestRunStarter = Backbone.View.extend({
 var TestRunStatusHeader = Backbone.View.extend({
 	initialize: function() {
 		var model = this.model;
-
 		$("#new-test-run").on("click", function() {
 			console.log("Triggering event", model);
 			model.trigger("showView", "default");
--- chrome/js/test_runner/modules/test_runs/TestRunStatusHeader.js
+++ chrome/js/test_runner/modules/test_runs/TestRunStatusHeader.js
@@ -1,7 +1,6 @@
 var TestRunStatusHeader = Backbone.View.extend({
 	initialize: function() {
 		var model = this.model;
-
 		$("#new-test-run").on("click", function() {
 			console.log("Triggering event", model);
 			model.trigger("showView", "default");
